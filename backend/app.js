ff14abada87a5201f2f89f9236c8a58e62cc3ea5
PopielarzManon <popielarzmanon@gmail.com>
2021-07-21 17:37:06 +0200

    :construction: implémentation crud

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
+const Thing = require('./models/Thing');
 
-const app = express();
 
-mongoose.connect('mongodb+srv://user_01:vytalu1058@cluster0.keawd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+mongoose.connect('mongodb+srv://user_01:vytalu1058@cluster0.keawd.mongodb.net/Cluster0?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  const app = express();
 
 
 app.use((req, res, next) => {
@@ -38,9 +40,28 @@ app.post('/api/stuff', (req, res, next) => {
       .catch(error => res.status(404).json({ error }));
   });
 
-  app.use('/api/stuff', (req, res, next) => {
+  app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+  });
+
+  app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.get('/api/stuff', (req, res, next) => {
     Thing.find()
       .then(things => res.status(200).json(things))
       .catch(error => res.status(400).json({ error }));
   });
+
   module.exports = app;
